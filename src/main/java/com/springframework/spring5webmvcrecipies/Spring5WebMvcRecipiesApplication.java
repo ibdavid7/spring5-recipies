45da73bf614f8b7d289af318167d955382b38417
David <ibdavid@gmx.net>
2021-09-25 17:06:51 -0500

    Initial commit

---
+++ src/main/java/com/springframework/spring5webmvcrecipies/Spring5WebMvcRecipiesApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5webmvcrecipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebMvcRecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebMvcRecipiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/spring5webmvcrecipies/Spring5WebMvcRecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5webmvcrecipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebMvcRecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
